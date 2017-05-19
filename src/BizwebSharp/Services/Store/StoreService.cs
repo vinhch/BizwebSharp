2d3470579fe4898ad07d7a646feea904f8d6fcd7
vinhch <vinhomn@gmail.com>
2017-05-20 00:48:26 +0700

    add StoreService

---
+++ src/BizwebSharp/Services/Store/StoreService.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using BizwebSharp.Entities;
+using BizwebSharp.Infrastructure;
+
+namespace BizwebSharp.Services
+{
+    public class StoreService : BaseService
+    {
+        public StoreService(BizwebAuthorizationState authState) : base(authState)
+        {
+        }
+
+        public virtual async Task<Store> GetAsync(string fields = null)
+        {
+            return await MakeRequest<Store>("store.json", HttpMethod.GET, "store", new { fields });
+        }
+
+        public virtual async Task UninstallAppAsync()
+        {
+            await MakeRequest("api_permissions/current.json", HttpMethod.DELETE);
+        }
+    }
+}
