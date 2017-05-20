bae9402f871a83bb16ffc0816862741698626235
vinhch <vinhomn@gmail.com>
2017-05-21 00:25:01 +0700

    add missing ToSnakeCase method

---
+++ src/BizwebSharp/Extensions/StringExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BizwebSharp.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string ToSnakeCase(this string input)
+        {
+            return string.Concat(input.Select((x, i) =>
+            {
+                var s = x.ToString();
+
+                if (!char.IsUpper(x))
+                {
+                    return s;
+                }
+
+                return i > 0 ? "_" + s.ToLower() : s.ToLower();
+            }));
+        }
+    }
+}
