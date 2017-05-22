d3664052c8b817c12c392994f8213bfdb93cff58
vinhch <vinhomn@gmail.com>
2017-05-22 19:17:09 +0700

    add related enums for Order

---
+++ src/BizwebSharp/Enums/DiscountType.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+using BizwebSharp.Converters;
+using Newtonsoft.Json;
+
+namespace BizwebSharp.Enums
+{
+    [JsonConverter(typeof(NullableEnumConverter<DiscountType>))]
+    public enum DiscountType
+    {
+        [EnumMember(Value = "percentage")]
+        Percentage,
+
+        [EnumMember(Value = "shipping")]
+        Shipping,
+
+        [EnumMember(Value = "fixed_amount")]
+        FixedAmount,
+
+        [EnumMember(Value = "none")]
+        None,
+    }
+}
---
+++ src/BizwebSharp/Enums/OrderFinancialStatus.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.Serialization;
+using BizwebSharp.Converters;
+using Newtonsoft.Json;
+
+namespace BizwebSharp.Enums
+{
+    [JsonConverter(typeof(NullableEnumConverter<OrderFinancialStatus>))]
+    public enum OrderFinancialStatus
+    {
+        [EnumMember(Value = "pending")]
+        Pending,
+
+        [EnumMember(Value = "authorized")]
+        Authorized,
+
+        [EnumMember(Value = "partially_paid")]
+        PartiallyPaid,
+
+        [EnumMember(Value = "paid")]
+        Paid,
+
+        [EnumMember(Value = "partially_refunded")]
+        PartiallyRefunded,
+
+        [EnumMember(Value = "refunded")]
+        Refunded,
+
+        [EnumMember(Value = "voided")]
+        Voided
+    }
+}
