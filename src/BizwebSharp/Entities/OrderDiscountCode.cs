56293879a6cec7f5bcde0268fc6e1e038141193f
vinhch <vinhomn@gmail.com>
2017-05-22 19:14:56 +0700

    add Order entities

---
+++ src/BizwebSharp/Entities/OrderDiscountCode.cs
@@ -0,0 +1,26 @@
+﻿using BizwebSharp.Enums;
+using Newtonsoft.Json;
+
+namespace BizwebSharp.Entities
+{
+    public class OrderDiscountCode
+    {
+        /// <summary>
+        /// The amount of the discount.
+        /// </summary>
+        [JsonProperty("amount")]
+        public string Amount { get; set; }
+
+        /// <summary>
+        /// The discount code.
+        /// </summary>
+        [JsonProperty("code")]
+        public string Code { get; set; }
+
+        /// <summary>
+        /// The type of discount. Known values are 'percentage', 'shipping', 'fixed_amount' and 'none'.
+        /// </summary>
+        [JsonProperty("type")]
+        public DiscountType? Type { get; set; }
+    }
+}
