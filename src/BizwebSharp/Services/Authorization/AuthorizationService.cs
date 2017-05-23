ec60b595dd80bd3dda98d885cad43ce2b12248bc
vinhch <vinhomn@gmail.com>
2017-05-23 21:59:31 +0700

    fix AuthorizationService because NameValueCollection does not exist in dotnet standard

--- src/BizwebSharp/Services/Authorization/AuthorizationService.cs
+++ src/BizwebSharp/Services/Authorization/AuthorizationService.cs
@@ -24,7 +24,7 @@ namespace BizwebSharp.Services.Authorization
             "timestamp"
         };
 
-        private static Func<string, bool, string> ReplaceChars { get; } = (s, isKey) =>
+        private static Func<string, bool, string> EncodeQuery { get; } = (s, isKey) =>
         {
             //Important: Replace % before replacing &. Else second replace will replace those %25s.
             var output = s?.Replace("%", "%25").Replace("&", "%26") ?? "";
@@ -34,6 +34,42 @@ namespace BizwebSharp.Services.Authorization
 
             return output;
         };
+
+        private static string PrepareQuerystring(NameValueCollection querystring, string joinWith)
+        {
+            var queryDictionary = new Dictionary<string, string>();
+            foreach (var item in querystring)
+            {
+                var key = (string)item;
+                var value = querystring[key];
+
+                if (!QueryKeyForHash.Contains(key)) continue;
+
+                queryDictionary.Add(EncodeQuery(key, true), EncodeQuery(value, false));
+            }
+            var kvps = queryDictionary.OrderBy(kvp => kvp.Key)
+                .Select(kvp => $"{kvp.Key}={kvp.Value}");
+
+            return string.Join(joinWith, kvps);
+        }
+
+        private static string PrepareQuerystring(IEnumerable<KeyValuePair<string, StringValues>> querystring, string joinWith)
+        {
+            var queryDictionary = new Dictionary<string, string>();
+            foreach (var kvp in querystring)
+            {
+                var key = kvp.Key;
+                var value = kvp.Value;
+
+                if (!QueryKeyForHash.Contains(key)) continue;
+
+                queryDictionary.Add(EncodeQuery(key, true), EncodeQuery(value, false));
+            }
+            var kvps = queryDictionary.OrderBy(kvp => kvp.Key)
+                .Select(kvp => $"{kvp.Key}={kvp.Value}");
+
+            return string.Join(joinWith, kvps);
+        }
 
         // To calculate HMAC signature:
         // 1. Cast querystring to KVP pairs (key-value pair).
@@ -51,17 +87,13 @@ namespace BizwebSharp.Services.Authorization
             double? requestTimestampSpan = null)
         {
             var hmac = querystring.Get("hmac");
-
             if (string.IsNullOrEmpty(hmac))
                 return false;
 
-            var kvps = querystring.Cast<string>()
-                .Select(s => new {Key = ReplaceChars(s, true), Value = ReplaceChars(querystring[s], false)})
-                .Where(kvp => QueryKeyForHash.Contains(kvp.Key))
-                .OrderBy(kvp => kvp.Key)
-                .Select(kvp => $"{kvp.Key}={kvp.Value}");
+            var kvps = PrepareQuerystring(querystring, "&");
+            var timestampInString = querystring.Get("timestamp");
 
-            return ValidateRequest(hmac, string.Join("&", kvps), apiSecretKey, querystring.Get("timestamp"),
+            return ValidateRequest(hmac, kvps, apiSecretKey, timestampInString,
                 requestTimestampSpan);
         }
 
@@ -69,38 +101,36 @@ namespace BizwebSharp.Services.Authorization
             string apiSecretKey,
             double? requestTimestampSpan = null)
         {
-            var hmac = querystring.FirstOrDefault(q => q.Key.ToLower() == "hmac").Value;
-
+            var hmac = querystring.FirstOrDefault(kvp => kvp.Key.ToLower() == "hmac").Value;
             if (string.IsNullOrEmpty(hmac))
                 return false;
 
-            var kvps = querystring.Where(kvp => QueryKeyForHash.Contains(kvp.Key))
-                .Select(s => new {Key = ReplaceChars(s.Key, true), Value = ReplaceChars(s.Value, false)})
-                .OrderBy(kvp => kvp.Key)
-                .Select(kvp => $"{kvp.Key}={kvp.Value}");
+            var kvps = PrepareQuerystring(querystring, "&");
+            var timestampInString = querystring.First(s => s.Key == "timestamp").Value;
 
-            return ValidateRequest(hmac, string.Join("&", kvps), apiSecretKey,
-                querystring.First(s => s.Key == "timestamp").Value,
+            return ValidateRequest(hmac, kvps, apiSecretKey, timestampInString,
                 requestTimestampSpan);
         }
 
-        public static bool ValidateRequest(string hmac, string kvpInString, string apiSecretKey,
+        public static bool ValidateRequest(string signature, string contentToCheck, string apiSecretKey,
             string timestampInString = null, double? requestTimestampSpan = null)
         {
             var hmacHasher = new HMACSHA256(Encoding.UTF8.GetBytes(apiSecretKey));
-            var hash = hmacHasher.ComputeHash(Encoding.UTF8.GetBytes(kvpInString));
+            var hash = hmacHasher.ComputeHash(Encoding.UTF8.GetBytes(contentToCheck));
 
             //Convert bytes back to string, replacing dashes, to get the final signature.
             var calculatedSignature = Convert.ToBase64String(hash);
 
-            //Request signature is valid if the calculated signature matches the signature from the querystring.
-            var isValidSignature =
-                string.Compare(hmac.ToUpper(), calculatedSignature.ToUpper(), StringComparison.Ordinal) == 0;
+            //Request signature is valid if the calculated signature matches the signature of content.
+            var isValidSignature = string.Equals(calculatedSignature, signature,
+                StringComparison.CurrentCultureIgnoreCase);
 
             //Check request timestamp span if need
             var isValidTimestamp = true;
             double timestamp;
-            if ((requestTimestampSpan != null) && double.TryParse(timestampInString, out timestamp))
+            if (!string.IsNullOrEmpty(timestampInString) &&
+                requestTimestampSpan != null &&
+                double.TryParse(timestampInString, out timestamp))
             {
                 var currentTimestamp = DateTime.UtcNow.ToUnixTimestamp();
                 isValidTimestamp = currentTimestamp - timestamp < requestTimestampSpan;
@@ -120,31 +150,24 @@ namespace BizwebSharp.Services.Authorization
             // To calculate signature, order all querystring parameters by alphabetical (exclude the
             // signature itself). Then, hash it with the secret key.
 
-            Func<string, bool, string> replaceChars = (s, isKey) =>
-            {
-                //Important: Replace % before replacing &. Else second replace will replace those %25s.
-                var output = s?.Replace("%", "%25").Replace("&", "%26") ?? "";
+            var kvps = PrepareQuerystring(querystring, string.Empty);
 
-                if (isKey)
-                    output = output.Replace("=", "%3D");
+            return ValidateRequest(signature, kvps, apiSecretKey);
+        }
 
-                return output;
-            };
+        public static bool IsAuthenticProxyRequest(IEnumerable<KeyValuePair<string, StringValues>> querystring, string apiSecretKey)
+        {
+            var signature = querystring.FirstOrDefault(kvp => kvp.Key.ToLower() == "signature").Value;
 
-            var kvps = querystring.Cast<string>()
-                .Select(s => new {Key = replaceChars(s, true), Value = replaceChars(querystring[s], false)})
-                .Where(kvp => (kvp.Key != "signature") && (kvp.Key != "hmac"))
-                .OrderBy(kvp => kvp.Key)
-                .Select(kvp => $"{kvp.Key}={kvp.Value}");
+            if (string.IsNullOrEmpty(signature))
+                return false;
 
-            var hmac = new HMACSHA256(Encoding.UTF8.GetBytes(apiSecretKey));
-            var hash = hmac.ComputeHash(Encoding.UTF8.GetBytes(string.Join(null, kvps)));
+            // To calculate signature, order all querystring parameters by alphabetical (exclude the
+            // signature itself). Then, hash it with the secret key.
 
-            //Convert bytes back to string, replacing dashes, to get the final signature.
-            var calculatedSignature = BitConverter.ToString(hash).Replace("-", "");
+            var kvps = PrepareQuerystring(querystring, string.Empty);
 
-            //Request is valid if the calculated signature matches the signature from the querystring.
-            return string.Equals(calculatedSignature, signature, StringComparison.CurrentCultureIgnoreCase);
+            return ValidateRequest(signature, kvps, apiSecretKey);
         }
 
         public static async Task<bool> IsAuthenticWebhookAsync(NameValueCollection requestHeaders, Stream inputStream,
@@ -167,25 +190,49 @@ namespace BizwebSharp.Services.Authorization
             var hmacHeader = requestHeaders.Get("X-Bizweb-Hmac-Sha256");
 
             if (string.IsNullOrEmpty(hmacHeader))
+            {
                 return false;
+            }
 
-            //Compute a hash from the apiKey and the request body
-            var hmac = new HMACSHA256(Encoding.UTF8.GetBytes(apiSecretKey));
-            var hash = Convert.ToBase64String(hmac.ComputeHash(Encoding.UTF8.GetBytes(requestBody)));
+            return ValidateRequest(hmacHeader, requestBody, apiSecretKey);
+        }
 
-            //Webhook is valid if computed hash matches the header hash
-            return hash == hmacHeader;
+        public static async Task<bool> IsAuthenticWebhookAsync(IEnumerable<KeyValuePair<string, StringValues>> requestHeaders,
+            Stream inputStream, string shopifySecretKey)
+        {
+            //Input stream may have already been read when a controller determines parameters to
+            //pass to an action. Reset position to 0.
+            inputStream.Position = 0;
+
+            //We do not dispose the StreamReader because disposing it will also dispose the input stream,
+            //and disposing a request's input stream can cause major headaches for the developer.
+            string requestBody = await new StreamReader(inputStream).ReadToEndAsync();
+
+            return IsAuthenticWebhook(requestHeaders, requestBody, shopifySecretKey);
+        }
+
+        public static bool IsAuthenticWebhook(IEnumerable<KeyValuePair<string, StringValues>> requestHeaders, string requestBody,
+            string apiSecretKey)
+        {
+            var hmacHeader = requestHeaders.FirstOrDefault(kvp => kvp.Key == "X-Bizweb-Hmac-Sha256").Value;
+
+            if (string.IsNullOrEmpty(hmacHeader))
+            {
+                return false;
+            }
+
+            return ValidateRequest(hmacHeader, requestBody, apiSecretKey);
         }
 
         public static Uri BuildAuthorizationUrl(IEnumerable<AuthorizationScope> scopes, string myApiUrl,
-            string apiKey, string redirectUri = null, string state = null)
+            string apiKey, string redirectUri, string state = null)
         {
             return BuildAuthorizationUrl(string.Join(",", scopes.Select(s => s.ToSerializedString())), myApiUrl,
                 apiKey, redirectUri, state);
         }
 
         public static Uri BuildAuthorizationUrl(string scopes, string myApiUrl,
-            string apiKey, string redirectUri = null, string state = null)
+            string apiKey, string redirectUri, string state = null)
         {
             //Prepare a uri builder for the tenant URL
             var builder = new UriBuilder(RequestEngine.BuildUri(myApiUrl));
@@ -194,14 +241,14 @@ namespace BizwebSharp.Services.Authorization
             var qs = new List<KeyValuePair<string, string>>
             {
                 new KeyValuePair<string, string>("client_id", apiKey),
-                new KeyValuePair<string, string>("scope", scopes)
+                new KeyValuePair<string, string>("scope", scopes),
+                new KeyValuePair<string, string>("redirect_uri", redirectUri)
             };
 
-            if (string.IsNullOrEmpty(redirectUri) == false)
-                qs.Add(new KeyValuePair<string, string>("redirect_uri", redirectUri));
-
             if (string.IsNullOrEmpty(state) == false)
+            {
                 qs.Add(new KeyValuePair<string, string>("state", state));
+            }
 
             builder.Path = "admin/oauth/authorize";
             builder.Query = string.Join("&", qs.Select(s => $"{s.Key}={s.Value}"));
@@ -212,11 +259,11 @@ namespace BizwebSharp.Services.Authorization
         public static async Task<string> AuthorizeAsync(string code, string myApiUrl, string apiKey,
             string apiSecretKey)
         {
-            var client = RequestEngine.CreateClient(new BizwebAuthorizationState {ApiUrl = myApiUrl});
+            var client = RequestEngine.CreateClient(new BizwebAuthorizationState { ApiUrl = myApiUrl });
             var req = RequestEngine.CreateRequest("oauth/access_token", Method.POST);
 
             //Build request body
-            req.AddJsonBody(new {client_id = apiKey, client_secret = apiSecretKey, code});
+            req.AddJsonBody(new { client_id = apiKey, client_secret = apiSecretKey, code });
 
             var response = await RequestEngine.ExecuteRequestAsync(client, req, new DefaultRequestExecutionPolicy());
 
