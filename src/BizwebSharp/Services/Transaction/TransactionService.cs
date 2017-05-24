15a303126bd3b646b82c99a86ced4927056005d7
vinhch <vinhomn@gmail.com>
2017-05-25 00:20:39 +0700

    add TransactionService

---
+++ src/BizwebSharp/Services/Transaction/TransactionService.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BizwebSharp.Entities;
+using BizwebSharp.Infrastructure;
+using BizwebSharp.Options;
+
+namespace BizwebSharp.Services
+{
+    public class TransactionService : BaseService
+    {
+        public TransactionService(BizwebAuthorizationState authState) : base(authState)
+        {
+        }
+
+        public virtual async Task<int> CountAsync(long orderId, ListOption option = null)
+        {
+            return await MakeRequest<int>($"orders/{orderId}/transactions/count.json", HttpMethod.GET, "count", option);
+        }
+
+        public virtual async Task<IEnumerable<Transaction>> ListAsync(long orderId, ListOption option = null)
+        {
+            return
+                await
+                    MakeRequest<List<Transaction>>($"orders/{orderId}/transactions.json", HttpMethod.GET, "transactions",
+                        option);
+        }
+
+        public virtual async Task<Transaction> GetAsync(long orderId, long transactionId, string fields = null)
+        {
+            dynamic options = null;
+            if (!string.IsNullOrEmpty(fields))
+            {
+                options = new { fields };
+            }
+
+            return
+                await
+                    MakeRequest<Transaction>($"orders/{orderId}/transactions/{transactionId}.json", HttpMethod.GET, "transaction",
+                        options);
+        }
+
+        public virtual async Task<Transaction> CreateAsync(long orderId, Transaction inputObject)
+        {
+            var root = new Dictionary<string, object>
+            {
+                {"transaction", inputObject}
+            };
+
+            return await MakeRequest<Transaction>($"orders/{orderId}/transactions.json", HttpMethod.POST, "transaction", root);
+        }
+
+        public virtual async Task<Transaction> UpdateAsync(long orderId, long transactionId, Transaction inputObject)
+        {
+            var root = new Dictionary<string, object>
+            {
+                {"transaction", inputObject}
+            };
+            return
+                await
+                    MakeRequest<Transaction>($"orders/{orderId}/transactions/{transactionId}.json", HttpMethod.PUT,
+                        "transaction", root);
+        }
+
+        public virtual async Task DeleteAsync(long orderId, long transactionId)
+        {
+            await MakeRequest($"orders/{orderId}/transactions/{transactionId}.json", HttpMethod.DELETE);
+        }
+    }
+}
