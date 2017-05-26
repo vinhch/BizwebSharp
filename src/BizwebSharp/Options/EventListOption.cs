47784d4d6b9610ebbf90d75df991799992a67f37
vinhch <vinhomn@gmail.com>
2017-05-26 22:54:06 +0700

    add EventService

---
+++ src/BizwebSharp/Options/EventListOption.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace BizwebSharp.Options
+{
+    public class EventListOption : ListOption
+    {
+        /// <summary>
+        /// Only show events specified in filter (comma , separated). A full list of events can be found at https://help.shopify.com/api/reference/event
+        /// </summary>
+        [JsonProperty("filter")]
+        public string Filters { get; set; }
+
+        /// <summary>
+        /// Only show events of a certain kind (comma , separated). A full list of events can be found at https://help.shopify.com/api/reference/event
+        /// </summary>
+        [JsonProperty("verb")]
+        public string Verbs { get; set; }
+    }
+}
---
+++ src/BizwebSharp/Services/Event/EventService.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BizwebSharp.Entities;
+using BizwebSharp.Infrastructure;
+using BizwebSharp.Options;
+
+namespace BizwebSharp.Services
+{
+    public class EventService : BaseService
+    {
+        public EventService(BizwebAuthorizationState authState) : base(authState)
+        {
+        }
+
+        public virtual async Task<int> CountAsync(EventListOption option = null)
+        {
+            return await MakeRequest<int>("events/count.json", HttpMethod.GET, "count", option);
+        }
+
+        public virtual async Task<IEnumerable<Event>> ListAsync(EventListOption option = null)
+        {
+            return
+                await
+                    MakeRequest<List<Event>>("events.json", HttpMethod.GET, "event", option);
+        }
+
+        public virtual async Task<Event> GetAsync(long id, string fields = null)
+        {
+            dynamic options = null;
+            if (!string.IsNullOrEmpty(fields))
+            {
+                options = new { fields };
+            }
+
+            return await MakeRequest<Event>($"events/{id}.json", HttpMethod.GET, "event", options);
+        }
+
+        public virtual async Task<IEnumerable<Event>> ListAsync(string subjectType, long subjectId,
+            EventListOption option = null)
+        {
+            return
+                await
+                    MakeRequest<List<Event>>($"{subjectType}/{subjectId}/events.json", HttpMethod.GET, "event", option);
+        }
+    }
+}
