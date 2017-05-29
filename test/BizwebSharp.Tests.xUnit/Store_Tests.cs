9f7aa217e4435e51e20e4bc07f06cb9f364bb3cc
vinhomn <vinhomn@gmail.com>
2017-05-29 16:16:15 +0700

    add Store_Tests

---
+++ test/BizwebSharp.Tests.xUnit/Store_Tests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using BizwebSharp.Services;
+using FluentAssertions;
+using Xunit;
+
+namespace BizwebSharp.Tests.xUnit
+{
+    [Trait("Category", "Store")]
+    public class Store_Tests : IDisposable
+    {
+        private StoreService Service => new StoreService(Utils.AuthState);
+
+        public void Dispose()
+        {
+        }
+
+        [Fact(DisplayName = "When getting a shop")]
+        public async Task Gets_Shops()
+        {
+            var store = await Service.GetAsync();
+
+            Assert.NotNull(store);
+            store.Name.Should().NotBeNullOrEmpty();
+            store.BizwebDomain.Should().NotBeNullOrEmpty();
+        }
+    }
+}
---
+++ test/BizwebSharp.Tests.xUnit/Utils.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using BizwebSharp.Infrastructure;
+using Microsoft.Extensions.Configuration;
+
+namespace BizwebSharp.Tests.xUnit
+{
+    public static class Utils
+    {
+        private static IConfigurationRoot _configuration;
+        public static IConfigurationRoot Configuration
+        {
+            get
+            {
+                if (_configuration == null)
+                {
+                    var builder = new ConfigurationBuilder()
+                        //.SetBasePath(env.ContentRootPath)
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                        .AddJsonFile("appsettings.private.json", optional: true);
+                        //.AddEnvironmentVariables();
+
+                    _configuration = builder.Build();
+                }
+                return _configuration;
+            }
+        }
+
+        private static BizwebAuthorizationState _authState;
+        public static BizwebAuthorizationState AuthState
+        {
+            get
+            {
+                if (_authState == null)
+                {
+                    _authState = new BizwebAuthorizationState();
+                    Configuration.GetSection("BizwebAuthorizationState").Bind(_authState);
+                }
+
+                if (string.IsNullOrEmpty(_authState.AccessToken) || string.IsNullOrEmpty(_authState.ApiUrl))
+                {
+                    throw new Exception($"{nameof(BizwebAuthorizationState)} was not initialize.");
+                }
+
+                return _authState;
+            }
+        }
+    }
+}
