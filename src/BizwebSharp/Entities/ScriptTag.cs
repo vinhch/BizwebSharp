ab0757a69c912ae97fd95acbb8513edee11224ad
vinhomn <vinhomn@gmail.com>
2017-06-01 17:13:19 +0700

    add ScriptTag_Tests

--- src/BizwebSharp/Entities/ScriptTag.cs
+++ src/BizwebSharp/Entities/ScriptTag.cs
@@ -5,11 +5,11 @@ namespace BizwebSharp.Entities
 {
     public class ScriptTag : BaseEntityWithTimeline
     {
-        /// <summary>
-        /// Where the script tag should be included on the store. Known values are 'online_store', 'order_status' or 'all'. Defaults to 'all'.
-        /// </summary>
-        [JsonProperty("display_scope")]
-        public string DisplayScope { get; set; }
+        ///// <summary>
+        ///// Where the script tag should be included on the store. Known values are 'online_store', 'order_status' or 'all'. Defaults to 'all'.
+        ///// </summary>
+        //[JsonProperty("display_scope")]
+        //public string DisplayScope { get; set; }
 
         /// <summary>
         /// DOM event which triggers the loading of the script. The only known value is 'onload'.
