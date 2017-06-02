225ddbc66f3c0626d848b041afa7ce70c50ac472
vinhch <vinhomn@gmail.com>
2017-06-03 00:22:15 +0700

    add Discount_Tests

---
+++ test/BizwebSharp.Tests.xUnit/Discount_Tests.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using BizwebSharp.Entities;
+using BizwebSharp.Enums;
+using BizwebSharp.Services;
+using Xunit;
+
+namespace BizwebSharp.Tests.xUnit
+{
+    [Trait("Category", "Discount")]
+    public class Discount_Tests
+    {
+        private DiscountService _Service => new DiscountService(Utils.AuthState);
+
+        [Fact(DisplayName = "Create An Application Credit",
+            Skip = "Discount API requires a Shopify Plus account, which is difficult to use when testing a lib.")]
+        public async Task Creates_An_Application_Credit()
+        {
+            var created = await _Service.CreateAsync(new Discount
+            {
+                DiscountType = DiscountType.FixedAmount,
+                Value = "10.00",
+                Code = "AuntieDot",
+                MinimumOrderAmount = "40.00",
+            });
+
+            Assert.NotNull(created);
+        }
+    }
+}
