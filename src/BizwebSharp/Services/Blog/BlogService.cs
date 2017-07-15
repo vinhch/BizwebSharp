25072662fd6b8603a10754229986dd24f592a967
vinhch <vinhomn@gmail.com>
2017-07-16 00:15:17 +0700

    change BlogService to inheritance BaseServiceWithSimpleCRUD

--- src/BizwebSharp/Services/Blog/BlogService.cs
+++ src/BizwebSharp/Services/Blog/BlogService.cs
@@ -6,60 +6,10 @@ using BizwebSharp.Options;
 
 namespace BizwebSharp
 {
-    public class BlogService : BaseService
+    public class BlogService : BaseServiceWithSimpleCRUD<Blog, BlogOption>
     {
         public BlogService(BizwebAuthorizationState authState) : base(authState)
         {
-        }
-
-        public virtual async Task<int> CountAsync(BlogOption options = null)
-        {
-            return await MakeRequest<int>("blogs/count.json", HttpMethod.GET, "count", options);
-        }
-
-        public virtual async Task<IEnumerable<Blog>> ListAsync(BlogOption options = null)
-        {
-            return await MakeRequest<List<Blog>>("blogs.json", HttpMethod.GET, "blogs", options);
-        }
-
-        public virtual async Task<Blog> GetAsync(long id, string fields = null)
-        {
-            dynamic option = null;
-            if (!string.IsNullOrEmpty(fields))
-            {
-                option = new { fields };
-            }
-
-            return await MakeRequest<Blog>($"blogs/{id}.json", HttpMethod.GET, "blog", option);
-        }
-
-        public virtual async Task<Blog> CreateAsync(Blog blog, IEnumerable<MetaField> metafields = null)
-        {
-            var body = blog.ToDictionary();
-
-            if (metafields != null && metafields.Any())
-            {
-                body["metafields"] = metafields;
-            }
-
-            return await MakeRequest<Blog>($"blogs.json", HttpMethod.POST, "blog", new {blog = body});
-        }
-
-        public virtual async Task<Blog> UpdateAsync(long blogId, Blog blog, IEnumerable<MetaField> metafields = null)
-        {
-            var body = blog.ToDictionary();
-
-            if (metafields != null && metafields.Any())
-            {
-                body["metafields"] = metafields;
-            }
-
-            return await MakeRequest<Blog>($"blogs/{blogId}.json", HttpMethod.PUT, "blog", new {blog = body});
-        }
-
-        public virtual async Task DeleteAsync(long id)
-        {
-            await MakeRequest($"blogs/{id}.json", HttpMethod.DELETE);
         }
     }
 }
