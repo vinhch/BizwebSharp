cff0c19f9d3f194d490837e8827b73b31ec1adeb
Vinh Cao <vinhomn@gmail.com>
2018-02-06 16:03:15 +0700

    PriceRule service

---
+++ src/BizwebSharp/Options/PriceRuleOption.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace BizwebSharp.Options
+{
+    public class PriceRuleOption : ListOption
+    {
+        /// <summary>
+        /// Show price rules starting after date.
+        /// </summary>
+        [JsonProperty("starts_on_min")]
+        public DateTimeOffset? StartsOnMin { get; set; }
+
+        /// <summary>
+        /// Show price rules starting before date.
+        /// </summary>
+        [JsonProperty("starts_on_max")]
+        public DateTimeOffset? StartsOnMax { get; set; }
+
+        /// <summary>
+        /// Show price rules ending after date.
+        /// </summary>
+        [JsonProperty("ends_on_min")]
+        public DateTimeOffset? EndsOnMin { get; set; }
+
+        /// <summary>
+        /// Show price rules ending before date.
+        /// </summary>
+        [JsonProperty("ends_on_max")]
+        public DateTimeOffset? EndsOnMax { get; set; }
+
+        /// <summary>
+        /// Show price rules with times used.
+        /// </summary>
+        [JsonProperty("times_used")]
+        public int? TimesUsed { get; set; }
+    }
+}
---
+++ src/BizwebSharp/Services/PriceRule/PriceRuleService.cs
@@ -0,0 +1,12 @@
+﻿using BizwebSharp.Infrastructure;
+using BizwebSharp.Options;
+
+namespace BizwebSharp.Services
+{
+    public class PriceRuleService : BaseServiceWithSimpleCRUD<PriceRule, PriceRuleOption>
+    {
+        protected PriceRuleService(BizwebAuthorizationState authState) : base(authState)
+        {
+        }
+    }
+}
