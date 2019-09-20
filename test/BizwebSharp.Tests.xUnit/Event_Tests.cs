af68cc1ced244763135c5d6d1de1a6d061001984
vinhomn <vinhomn@gmail.com>
2019-09-20 16:03:34 +0700

    Fix Events tests, need to create at least one order to test event.

--- test/BizwebSharp.Tests.xUnit/Event_Tests.cs
+++ test/BizwebSharp.Tests.xUnit/Event_Tests.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using BizwebSharp.Enums;
+using BizwebSharp.Infrastructure;
 using BizwebSharp.Options;
 using Xunit;
 
@@ -35,15 +39,9 @@ namespace BizwebSharp.Tests.xUnit
         [Fact(DisplayName = "List Events For Subjects")]
         public async Task Lists_Events_For_Subjects()
         {
-            // Get an order id
-            var orderId = (await new OrderService(Utils.AuthState).ListAsync(new OrderOption
-            {
-                Limit = 1
-            })).First().Id.Value;
-
             const string subject = "order";
             const string subjectType = "orders";
-            var list = await Fixture.Service.ListAsync(subjectType, orderId);
+            var list = await Fixture.Service.ListAsync(subjectType, Fixture.OrderId.Value);
 
             Assert.NotNull(list);
             Assert.All(list, e => Assert.Equal(subject, e.SubjectType));
@@ -68,10 +66,126 @@ namespace BizwebSharp.Tests.xUnit
         }
     }
 
-    public class Event_Tests_Fixture
+    public class Event_Tests_Fixture : IAsyncLifetime
     {
+        public async Task InitializeAsync()
+        {
+            // Create at least one Order for Event tests.
+            OrderId = (await CreateAOrder()).Id.Value;
+        }
+
+        public async Task DisposeAsync()
+        {
+            foreach (var order in CreatedOrders)
+            {
+                try
+                {
+                    await OrderService.DeleteAsync(order.Id.Value);
+                }
+                catch (BizwebSharpException ex)
+                {
+                    if (ex.HttpStatusCode != HttpStatusCode.NotFound)
+                    {
+                        Console.WriteLine($"Failed to delete Order with id {order.Id.Value}. {ex.Message}");
+                    }
+                }
+            }
+        }
+
+        private async Task CleanUpOrdersSetup()
+        {
+            foreach (var order in CreatedOrders)
+            {
+                try
+                {
+                    await OrderService.DeleteAsync(order.Id.Value);
+                }
+                catch (BizwebSharpException ex)
+                {
+                    if (ex.HttpStatusCode != HttpStatusCode.NotFound)
+                    {
+                        Console.WriteLine($"Failed to delete Order with id {order.Id.Value}. {ex.Message}");
+                    }
+                }
+            }
+
+            var customerService = new CustomerService(Utils.AuthState);
+            foreach (var obj in CreatedCustomers)
+            {
+                try
+                {
+                    await customerService.DeleteAsync(obj.Id.Value);
+                }
+                catch (BizwebSharpException ex)
+                {
+                    if (ex.HttpStatusCode != HttpStatusCode.NotFound)
+                    {
+                        Console.WriteLine($"Failed to delete created Customer with id {obj.Id.Value}. {ex.Message}");
+                    }
+                }
+            }
+        }
+
+        public long? OrderId { get; set; }
+
         public EventService Service => new EventService(Utils.AuthState);
 
-        public List<Event> Created { get; } = new List<Event>();
+        public OrderService OrderService => new OrderService(Utils.AuthState);
+
+        public List<Order> CreatedOrders { get; } = new List<Order>();
+
+        public List<Customer> CreatedCustomers { get; } = new List<Customer>();
+
+        public async Task<Order> CreateAOrder(bool skipAddToCreatedList = false)
+        {
+            var obj = await OrderService.CreateAsync(new Order()
+            {
+                CreatedOn = DateTime.UtcNow,
+                BillingAddress = new Address()
+                {
+                    Address1 = "123 4th Street",
+                    City = "Minneapolis",
+                    Province = "Minnesota",
+                    ProvinceCode = "MN",
+                    Zip = "55401",
+                    Phone = "555-555-5555",
+                    FirstName = "John",
+                    LastName = "Doe",
+                    Company = "Tomorrow Corporation",
+                    Country = "United States",
+                    CountryCode = "US",
+                    Default = true,
+                },
+                LineItems = new List<LineItem>()
+                {
+                    new LineItem()
+                    {
+                        Name = "Test Line Item",
+                        Title = "Test Line Item Title",
+                        Quantity = 2,
+                        Price = 5
+                    },
+                    new LineItem()
+                    {
+                        Name = "Test Line Item 2",
+                        Title = "Test Line Item Title 2",
+                        Quantity = 2,
+                        Price = 5
+                    }
+                },
+                FinancialStatus = OrderFinancialStatus.Paid,
+                TotalPrice = 5.00m,
+                Email = Guid.NewGuid() + "@example.com",
+                Note = "This order was created while testing BizwebSharp!",
+            });
+
+            if (!skipAddToCreatedList)
+            {
+                CreatedOrders.Add(obj);
+                CreatedCustomers.Add(obj.Customer);
+            }
+
+            return obj;
+        }
     }
 }
