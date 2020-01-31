edca78352ba94248bd4d63c44252b3aad5a7a9e2
vinhomn <vinhomn@gmail.com>
2020-01-31 23:57:12 +0700

    Modify build script to support linux compatibility, fix strong-named friend assembly issue with release build.

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,16 +1,32 @@
-$rootFolder = Resolve-Path "$PSScriptRoot/.."
-$projectFile = "$rootFolder/src/BizwebSharp/BizwebSharp.csproj"
-$artifactFolder = "$rootFolder/artifact"
+function Join-Paths {
+    if ($args.Count -eq 0) {
+        throw "Must supply some arguments."
+    }
+    $output = $args[0]
+    if ($args.Count -gt 1) {
+        foreach($path in $args[1..($args.Count - 1)]) {
+            $output = Join-Path $output -ChildPath $path
+        }
+    }
+    return $output
+}
+
+$rootFolder = Join-Paths $PSScriptRoot ".."
+$projectFile = Join-Paths $rootFolder "src" "BizwebSharp" "BizwebSharp.csproj"
+$artifactFolder = Join-Paths $rootFolder "artifact"
 
 if (Test-Path $artifactFolder -PathType Container) {
     Remove-Item $artifactFolder -Recurse
 }
 New-Item -ItemType Directory -Force -Path $artifactFolder
 
-dotnet clean -c Release $projectFile
+# build configuration {Debug|Release}
+$buildConfig = "Release"
+
+dotnet clean -c $buildConfig $projectFile
 dotnet restore $projectFile
-dotnet build -c Release $projectFile
+dotnet build -c $buildConfig $projectFile
 dotnet pack --no-build -c Release -o $artifactFolder $projectFile
 
-$nupkg = (Get-ChildItem $artifactFolder/*.nupkg)[0];
+$nupkg = (Get-ChildItem (Join-Paths $artifactFolder "*.nupkg"))[0];
 Write-Host $nupkg
--- src/BizwebSharp/Properties/AssemblyInfo.cs
+++ src/BizwebSharp/Properties/AssemblyInfo.cs
@@ -1,5 +1,7 @@
 ﻿using System.Runtime.CompilerServices;
 
+#if (DEBUG)
 // Allow "Friend" assemblies access to internals
 // https://docs.microsoft.com/en-us/dotnet/standard/assembly/friend
 [assembly: InternalsVisibleTo("BizwebSharp.Tests.xUnit")]
+#endif
